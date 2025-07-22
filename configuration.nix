6b722d4d794ca74a962f100b214ca4b4458b8b35
Yui Nishimura <nisshi.yui79@gmail.com>
2025-07-22 18:17:36 +0200

    muripo

--- configuration.nix
+++ configuration.nix
@@ -3,28 +3,15 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   users.users.alice = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ and Docker access for the user.
-    createHome = false; # Home dir provided by bind mount
+    extraGroups = [ "wheel" ];
     initialPassword = "test";
-  };
-
-  environment.systemPackages = with pkgs; [
-    cowsay
-    lolcat
-    docker # Docker client if needed outside of the service
-  ];
-
-  # Enable Docker daemon (rootful). For rootless, use virtualisation.docker.rootless.enable
-  virtualisation.docker.enable = true;
-
-  # Bind-mount the host workspace directory as Alice's home
-  fileSystems."/home/alice" = {
-    device = "/workspace";
-    fsType = "none"; # bind mount
-    options = [ "bind" "x-systemd.requires-mounts-for=/workspace" ];
-    neededForBoot = true; # mount early
   };
 
   system.stateVersion = "24.05";
